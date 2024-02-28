d28199c4a4849b6b1d5f28b2a821846ba34e1e68
CodeWithYaswanth <yaswanth.emailme@gmail.com>
2024-02-29 02:13:00 +0530

    commit9

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navigation from "./Components/Navigation"
+import YashSection  from "./Components/Yash"
+
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <YashSection />
+    </div>
+  );
+};
+
+export default App;
+ 
